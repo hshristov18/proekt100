5d4bc61e7b405b44d825571b474973a75d94c141
Hristo Hristov <hshristov18@codingburgas.bg>
2022-12-01 10:27:30 +0200

    sbygxfunbv

---
+++ src/main/java/com/springdemo/proekt100/Proekt100Application.java
@@ -0,0 +1,14 @@
+package com.springdemo.proekt100;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proekt100Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Proekt100Application.class, args);
+    }
+
+}
